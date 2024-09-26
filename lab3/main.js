aca04435f58232906ab967e8443d00fce2000c85
Uliana Ksondz bc2024-3 <uliana.ksiondz764@gmail.com>
2024-09-26 21:00:00 +0300

    Add commander.js for command-line arguments

--- lab3/main.js
+++ lab3/main.js
@@ -1,1 +1,11 @@
 console.log("The program works correctly!");
+const { Command } = require('commander');
+const program = new Command();
+
+program
+    .version('1.0.0')
+    .description('Program for processing NBU data')
+    .option('-f, --file <type>', 'Specify the data file', 'data.json')
+    .parse(process.argv);
+
+console.log(`Using data file: ${program.opts().file}`);
